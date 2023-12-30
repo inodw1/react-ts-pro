72ce47d4729db883aff4db2cfb8b1837fae255a5
inodw1 <inodwagachchi@gmail.com>
2023-12-30 16:05:35 +0530

    Create the base setup for the context api

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import Button from './UI/Button.tsx';
+import Button from "./UI/Button.tsx";
 
 export default function Header() {
-  return (
-    <header>
-      <h1>ReactTimer</h1>
+    return (
+        <header>
+            <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
-    </header>
-  );
+            <Button>Stop Timers</Button>
+        </header>
+    );
 }
